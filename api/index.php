d74aac46e830cb4c7c22273852e4e358ad9f38f7
Thien Ngoc <ngocnt@funtap.vn>
2020-08-12 14:55:11 +0700

    kkk

--- api/index.php
+++ api/index.php
@@ -1,15 +1,32 @@
 <?php
 
 $access_token = "EAADl7p3EviUBAH3aFW4k933PVdNzWZCkB2ievsGDZC6f8j2ECNOkSVhxzPZAiyq2swQsY7ZAyjaLXflBkkLgzPU1jgZCnOtHC2yv3bAbRccdpMkgGu0bXdCsvOn1MyvCGj1ySgobZAAID7XZBb7Qta9FHoBGq3ucr82eU5Qtye00SO507vMsZBev";
-$verify_token = "fb_time_bot";
-$hub_verify_token = null;
-if (isset($_REQUEST['hub_challenge'])) {
-    $challenge = $_REQUEST['hub_challenge'];
-    $hub_verify_token = $_REQUEST['hub_verify_token'];
-}
-if ($hub_verify_token === $verify_token) {
-    echo $challenge;
-}
+$hubVerifyToken = "fb_time_bot";
 
-$input = json_decode(file_get_contents("php://input"), true);
-echo $input;
+// check token at setup
+if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
+    echo $_REQUEST['hub_challenge'];
+    exit;
+}
+// handle bot's anwser
+$input = json_decode(file_get_contents('php://input'), true);
+$senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
+$messageText = $input['entry'][0]['messaging'][0]['message']['text'];
+$response = null;
+//set Message
+if ($messageText == "hi") {
+    $answer = "Hello";
+}
+//send message to facebook bot
+$response = [
+    'recipient' => ['id' => $senderId],
+    'message' => ['text' => $answer]
+];
+$ch = curl_init('https://graph.facebook.com/v2.6/me/messages?access_token=' . $accessToken);
+curl_setopt($ch, CURLOPT_POST, 1);
+curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($response));
+curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+if (!empty($input)) {
+    $result = curl_exec($ch);
+}
+curl_close($ch);
