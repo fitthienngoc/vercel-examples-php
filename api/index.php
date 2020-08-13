2410437d1f7694c2bc61db6ed71ab5f68bdc86a5
Thien Ngoc <ngocnt@funtap.vn>
2020-08-13 15:48:45 +0700

    up

--- api/index.php
+++ api/index.php
@@ -1,5 +1,14 @@
 <?php
 
+function console_log($output, $with_script_tags = true) {
+    $js_code = 'console.log(' . json_encode($output, JSON_HEX_TAG) . 
+');';
+    if ($with_script_tags) {
+        $js_code = '<script>' . $js_code . '</script>';
+    }
+    echo $js_code;
+}
+
 $access_token = "EAADl7p3EviUBAH3aFW4k933PVdNzWZCkB2ievsGDZC6f8j2ECNOkSVhxzPZAiyq2swQsY7ZAyjaLXflBkkLgzPU1jgZCnOtHC2yv3bAbRccdpMkgGu0bXdCsvOn1MyvCGj1ySgobZAAID7XZBb7Qta9FHoBGq3ucr82eU5Qtye00SO507vMsZBev";
 $hubVerifyToken = "fb_time_bot";
 
@@ -9,10 +18,13 @@ if ($_REQUEST['hub_verify_token'] === $hubVerifyToken) {
     exit;
 }
 
-if ($_REQUEST) {
-    var_dump($_REQUEST); 
+if (isset($_REQUEST)) {
+    console_log($_REQUEST);
     exit;
 }
+
+
+
 // // handle bot's anwser
 // $input = json_decode(file_get_contents('php://input'), true);
 // $senderId = $input['entry'][0]['messaging'][0]['sender']['id'];
